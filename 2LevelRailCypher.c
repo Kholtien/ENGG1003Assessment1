6836944631102e237c8a28e2dbf8622d97adf2a3
Colton Millington <c.millington@hotmail.ca>
2020-04-28 22:18:30 +1000

    Algorithm now completely works for Any message under 1000. Tested with messages of varying lengths and different rails. Tried with 4,2. 8,4. 6,3 rails and they all work

--- 2LevelRailCypher.c
+++ 2LevelRailCypher.c
@@ -4,20 +4,34 @@
 #include <math.h>
 //function prototypes
 const char* padMessage2Rail(const char* message,int a, int b);
-const char* rail2(const char* message,int a, int b);
+const char* rail2encrypt(const char* message,int a, int b);
 
 int main (void)
 {
-    char message[] = "HOLY COW THIS MESSAGE IS EVEN BETTER";
-    int  a = 6, b = 3;
-    const char* paddedMessage = padMessage2Rail(message,a,b);
+    const int charLim = 1000;
 
-    printf("%s\n",paddedMessage);
 
-    char testMsg[] = "HOLY_COW_THIS_MESSAGE_IS_EVEN_BETTERNGZAEOG";
-    const char* encryptedMessage = rail2(paddedMessage,6,3);
+    printf("Enter a phrase (under %d characters long)\n",charLim);
+    char input[charLim];
+    scanf("%[^\n]%*c", input);
+
+
+    printf("Enter The first key in the 2 level rail cipher:\n");
+    int key1;
+    scanf("%d", &key1);
+
+    
+    printf("Enter The second key in the 2 level rail cipher:\n");
+    int key2;
+    scanf("%d", &key2);
+
+//    const char* paddedMessage = padMessage2Rail(input,key1,key2);
+
+//    printf("%s\n",paddedMessage);
+
+    char testMsg[] = "HOLY_COW_THIS_MESSAGE_IS_EVEN_BETTERNGZAE";
+    const char* encryptedMessage = rail2encrypt(input,key1,key2);
     printf("%s\n",encryptedMessage);
-    free((char*)paddedMessage);
     free((char*)encryptedMessage);
 
     return 0;
@@ -30,6 +44,12 @@ const char* padMessage2Rail(const char* message,int a, int b)
     int oneCycle = 2*a + 2*b - 3; //number of letters in one 'cycle'
     int numCycles = (int)ceil((float)messageLen/oneCycle); //number of complete "W" shapes in the rail
     int numChars = numCycles * oneCycle - numCycles + 1; //this is the number of characters in the padded string
+    if (numChars < messageLen){
+        //the above algorithm for numCycles sometimes is one lower which causes the number of characters to be less than it should be in the 
+        //padded string. This will add another cycle thus increasing the number of characters to the right number. 
+        numCycles++;
+        numChars = numCycles * oneCycle - numCycles + 1;
+    }
     char messWithExtraChar[numChars + 1]; //this creates string array with the right number of characters plus the null character
 
     
@@ -49,53 +69,25 @@ const char* padMessage2Rail(const char* message,int a, int b)
 
     return out;
 }
-const char* rail2(const char* message,int a, int b)
+const char* rail2encrypt(const char* message,int a, int b)
 {
-    int  messageLen = strlen(message);
+    const char* paddedMessage = padMessage2Rail(message,a,b);
+    int  messageLen = strlen(paddedMessage);
 
     int oneCycle = 2*a + 2*b - 3; //number of letters in one 'cycle'
     int numCycles = (int)ceil((float)messageLen/oneCycle); //number of complete "W" shapes in the rail
     int numChars = numCycles * oneCycle - numCycles + 1; //this is the number of characters in the padded string
-//    int peakIndex[numCycles + 1]; //This array will contain the index numbers for the peaks
-//    int middlePeakIndex[numCycles]; //this array will cointain the index numbers of the middle peaks
-//    int basesIndex[2 * numCycles];
+    if (numChars < messageLen){
+        //the above algorithm for numCycles sometimes is one lower which causes the number of characters to be less than it should be in the 
+        //padded string. This will add another cycle thus increasing the number of characters to the right number. 
+        numCycles++;
+        numChars = numCycles * oneCycle - numCycles + 1;
+    }
+
     int encryptedIndexes[messageLen];
 
 
-
-
-//    char charArray[numChars][a];
-//    for(int i = 0; i < numChars; i++){
-//        for(int j = 0; j < a; j++){
-//            charArray[i][j] = (char)0;
-//        }
-//    }
-
-
-
-
-//    //setting the values of the peakIndex
-//    peakIndex[0] = 0; //first peak is always at 0
-//    for(int i = 0, j = 0; i < numCycles; i++, j += oneCycle - 1){
-//        peakIndex[i] = j;
-//    }
-//
-//    //setting the values of the middlePeakIndex
-//    //first middle peak is at the index of the first peak divided by 2 
-//    // (the index where the cipher first returns to the top rail)
-//    for(int i = peakIndex[1]/2, j = 0; i < numChars; i += peakIndex[1], j++){
-//        middlePeakIndex[j] = i;
-//    }
-//
-//    //setting values for basesIndex
-//    for(int i = 0, j = b - 1; i < 2 * numCycles; i += 2, j += oneCycle - 1){
-//        basesIndex[i] = j;
-//        basesIndex[i+1] = j + 2 * b - 2;
-//    }
-    
-
-
-
+//algorithm for merging the message onto a 1D array for the rail.
     for(int i = 0, curRail = 1, totRail = a, midRail = b; i <= messageLen; i++){
         
 
@@ -142,11 +134,11 @@ const char* rail2(const char* message,int a, int b)
         }
     }
     
-    
     char* out = malloc(numChars + 1);
-    for (int i = 0; i <= numChars; i++){
-        out[i] = message[encryptedIndexes[i] - 1];
+    for (int i = 0; i < numChars; i++){
+        out[i] = paddedMessage[encryptedIndexes[i] - 1];
     }
+    out[numChars] = 0;
 
     return out;
 }
